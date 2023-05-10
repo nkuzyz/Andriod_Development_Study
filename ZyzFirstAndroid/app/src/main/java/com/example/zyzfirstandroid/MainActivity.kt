d6a00395f4fd8077d77213de63be115ff7ed7117
张怡桢 <93150961+nkuzyz@users.noreply.github.com>
2023-05-10 21:45:51 +0800

    11

--- ZyzFirstAndroid/app/src/main/java/com/example/zyzfirstandroid/MainActivity.kt
+++ ZyzFirstAndroid/app/src/main/java/com/example/zyzfirstandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.zyzfirstandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -11,16 +12,21 @@ import android.widget.Toast
  * This activity allows the user to roll a dice and view the result
  * on the screen.
  */
+const val KEY_VALUE = 1
 class MainActivity : AppCompatActivity() {
+    private var diceRoll = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
-
-        // Do a dice roll when the app starts
-        rollDice()
+        if (savedInstanceState != null) {
+            diceRoll = savedInstanceState.getInt(KEY_VALUE.toString(), 1)
+        }
+        showImage(diceRoll)
+        Log.i("MainActivity", "onCreate Called")
     }
     /**
      * Roll the dice and update the screen with the result.
@@ -29,8 +35,12 @@ class MainActivity : AppCompatActivity() {
 
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
-        val diceRoll = dice.roll()
+        diceRoll = dice.roll()
+        showImage(diceRoll)
 
+    }
+
+    private fun showImage(diceRoll: Int){
         // Update the screen with the dice roll
         val resultTextView02: TextView = findViewById(R.id.textView2)
         resultTextView02.text = diceRoll.toString()
@@ -48,10 +58,17 @@ class MainActivity : AppCompatActivity() {
         diceImage.contentDescription = diceRoll.toString()
     }
 
+
+
     class Dice(private val numSides: Int) {
 
         fun roll(): Int {
             return (1..numSides).random()
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_VALUE.toString(), diceRoll)
     }
 }
