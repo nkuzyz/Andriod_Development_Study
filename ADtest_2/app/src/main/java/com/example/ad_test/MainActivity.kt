3fb7797782eaab1fd66d9bca3237174ce01d48ac
张怡桢 <93150961+nkuzyz@users.noreply.github.com>
2024-03-12 17:20:22 +0800

    can be used

--- ADtest_2/app/src/main/java/com/example/ad_test/MainActivity.kt
+++ ADtest_2/app/src/main/java/com/example/ad_test/MainActivity.kt
@@ -1,142 +1,222 @@
 package com.example.ad_test
+
 import android.Manifest
-import android.app.Activity
 import android.content.ContentValues
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
-import androidx.activity.ComponentActivity
-import android.widget.Button
-import android.widget.VideoView
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
+import androidx.camera.video.FallbackStrategy
+import androidx.camera.video.MediaStoreOutputOptions
+import androidx.camera.video.Quality
+import androidx.camera.video.QualitySelector
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.camera.video.VideoRecordEvent
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
+import androidx.lifecycle.lifecycleScope
+import com.example.ad_test.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 
-class MainActivity : ComponentActivity() {
-    private lateinit var previewView: PreviewView // 添加视频视图成员变量
-    private lateinit var sensorManagerHelper: SensorManagerHelper
-    private lateinit var cameraManagerHelper: CameraManagerHelper
+typealias LumaListener = (luma: Double) -> Unit
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+    private lateinit var cameraExecutor: ExecutorService
     private lateinit var permissionsManager: PermissionsManager
-
+    private lateinit var sensorManagerHelper: SensorManagerHelper
     private var recordingStartTime: Long = 0
+    private var recordingEndTime: Long = 0
 
 
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
-        previewView = findViewById(R.id.previewView)
-        //注册传感器类
-        sensorManagerHelper = SensorManagerHelper(this)
-        //注册相机类
-        cameraManagerHelper = CameraManagerHelper(this, this, previewView)
-
+        // Request camera permissions
         // 获得权限
         permissionsManager = PermissionsManager(this)
+        sensorManagerHelper = SensorManagerHelper(this)
 
         if (!permissionsManager.allPermissionsGranted()) {
             permissionsManager.requestPermissions()
-        } else {
-            // 执行需要权限的操作
         }
-        //打开摄像头预览
-        cameraManagerHelper.startCamera()
+        startCamera()
 
-        //按钮设置
-        val recordButton: Button = findViewById(R.id.record)
-        recordButton.setOnClickListener {
-//            requestCameraPermission()
-            if (cameraManagerHelper.isRecording) {
-                cameraManagerHelper.stopRecording()
-                recordButton.text = "RECORD"
-                sensorManagerHelper.stopSensorListener(sensorEventListener)
-            } else {
-                cameraManagerHelper.startRecording()
-                recordButton.text = "STOP"
-                sensorManagerHelper.startSensorListener(sensorEventListener) // 开始收集9轴数据
+        // Set up the listeners for take photo and video capture buttons
+        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+        viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun takePhoto() {}
+
+    // Implements VideoCapture use case, including start and stop capturing.
+    private fun captureVideo() {
+        val videoCapture = this.videoCapture ?: return
+
+
+        viewBinding.videoCaptureButton.isEnabled = false
+
+        val curRecording = recording
+        if (curRecording != null) {
+            // Stop the current recording session.
+            curRecording.stop()
+            recording = null
+            return
+        }
+
+        // create and start a new recording session
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
             }
         }
+
+        val mediaStoreOutputOptions = MediaStoreOutputOptions
+            .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+        recording = videoCapture.output
+            .prepareRecording(this, mediaStoreOutputOptions)
+            .apply {
+                if (PermissionChecker.checkSelfPermission(this@MainActivity,
+                        Manifest.permission.RECORD_AUDIO) ==
+                    PermissionChecker.PERMISSION_GRANTED)
+                {
+                    withAudioEnabled()
+                }
+            }
+            .start(ContextCompat.getMainExecutor(this)) { recordEvent ->
+                when(recordEvent) {
+                    is VideoRecordEvent.Start -> {
+                        recordingStartTime = System.currentTimeMillis() // 记录开始时间
+                        sensorManagerHelper.startSensorListener(sensorEventListener)
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.stop_capture)
+                            isEnabled = true
+                        }
+                    }
+                    is VideoRecordEvent.Finalize -> {
+                        sensorManagerHelper.stopSensorListener(sensorEventListener)
+                        recordingEndTime = System.currentTimeMillis() // 记录结束时间
+                        val recordingDuration = recordingEndTime - recordingStartTime // 计算录制时长
+                        if (!recordEvent.hasError()) {
+                            val msg = "Video capture succeeded: " +
+                                    "${recordEvent.outputResults.outputUri}, Duration: $recordingDuration ms"
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT)
+                                .show()
+                            Log.d(TAG, msg)
+                        } else {
+                            recording?.close()
+                            recording = null
+                            Log.e(TAG, "Video capture ends with error: " +
+                                    "${recordEvent.error}, Duration: $recordingDuration ms")
+                        }
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.start_capture)
+                            isEnabled = true
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+//            val qualitySelector = QualitySelector.from(Quality.HIGHEST, FallbackStrategy.higherQualityOrLowerThan(Quality.SD))
+            val recorder = Recorder.Builder()
+                .setQualitySelector(QualitySelector.from(Quality.LOWEST))
+                .build()
+            videoCapture = VideoCapture.withOutput(recorder)
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider
+                    .bindToLifecycle(this, cameraSelector, preview, videoCapture)
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 
 
-
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        permissionsManager.handlePermissionsResult(requestCode, permissions, grantResults,
-//            onGranted = {
-//                // 所有权限已被授予，继续执行需要权限的操作
-//            },
-//            onDenied = {
-//                // 用户拒绝了一些权限，可以在这里处理权限被拒绝的情况
-//            }
-//        )
-//    }
-//
-//    private fun requestCameraPermission() {
-//        when {
-//            ContextCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.CAMERA
-//            ) == PackageManager.PERMISSION_GRANTED -> {
-//                // 权限已经被授予，继续您的操作
-//                cameraManagerHelper.startRecording()//开始录像
-//                sensorManagerHelper.startSensorListener(sensorEventListener) // 开始收集9轴数据
-//            }
-//            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
-//                // 提供一个额外的说明，如果需要的话
-//                // 之后再次请求权限
-//                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-//            }
-//            else -> {
-//                // 直接请求权限
-//                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-//            }
-//        }
-//    }
-//    private val requestPermissionLauncher =
-//        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-//            if (isGranted) {
-//                // 权限被授予，继续操作
-//            } else {
-//                // 权限被拒绝，处理拒绝的情况
-//            }
-//        }
-
-
-
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
 
     private val sensorEventListener = object : SensorEventListener {
         override fun onSensorChanged(event: SensorEvent) {
-            // 只有在开始记录后才处理数据
             if (recordingStartTime > 0) {
-                val relativeTimeMillis = System.currentTimeMillis() - recordingStartTime
-                val dataString = "${event.values.joinToString(", ")}, Relative Timestamp: ${relativeTimeMillis}\n"
+                lifecycleScope.launch(Dispatchers.Default) { // 在后台线程处理数据
+                    val relativeTimeMillis = System.currentTimeMillis() - recordingStartTime
+                    val dataString = "${event.values.joinToString(", ")}, Relative Timestamp: ${relativeTimeMillis}\n"
 
-                when (event.sensor.type) {
-                    Sensor.TYPE_ACCELEROMETER -> sensorManagerHelper.writeDataToFileAcc (dataString)
-                    Sensor.TYPE_GYROSCOPE -> sensorManagerHelper.writeDataToFileGyro( dataString)
-                    Sensor.TYPE_MAGNETIC_FIELD -> sensorManagerHelper.writeDataToFileMag(dataString)
+                    when (event.sensor.type) {
+                        Sensor.TYPE_ACCELEROMETER -> sensorManagerHelper.writeDataToFileAcc(dataString)
+                        Sensor.TYPE_GYROSCOPE -> sensorManagerHelper.writeDataToFileGyro(dataString)
+                        Sensor.TYPE_MAGNETIC_FIELD -> sensorManagerHelper.writeDataToFileMag(dataString)
+                    }
                 }
             }
         }
 
         override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-            // 这里可以处理传感器精度变化，如果需要的话
+            // 可以在这里处理传感器精度变化，如果需要的话
         }
     }
 
 
-
 }
+
