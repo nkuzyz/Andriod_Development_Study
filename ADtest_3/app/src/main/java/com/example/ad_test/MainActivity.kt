61d9e46e1e2149ff041de4e976a61be27119f7fa
张怡桢 <93150961+nkuzyz@users.noreply.github.com>
2024-03-13 16:14:14 +0800

    111

---
+++ ADtest_3/app/src/main/java/com/example/ad_test/MainActivity.kt
@@ -0,0 +1,227 @@
+package com.example.ad_test
+
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.FallbackStrategy
+import androidx.camera.video.MediaStoreOutputOptions
+import androidx.camera.video.Quality
+import androidx.camera.video.QualitySelector
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.camera.video.VideoRecordEvent
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
+import androidx.lifecycle.lifecycleScope
+import com.example.ad_test.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
+typealias LumaListener = (luma: Double) -> Unit
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var permissionsManager: PermissionsManager
+    private lateinit var sensorManagerHelper: SensorManagerHelper
+    private var recordingStartTime: Long = 0
+    private var recordingEndTime: Long = 0
+//    private var latestAccData: FloatArray? = null
+//    private var latestGyroData: FloatArray? = null
+//    private var latestMagData: FloatArray? = null
+
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        // Request camera permissions
+        // 获得权限
+        permissionsManager = PermissionsManager(this)
+        sensorManagerHelper = SensorManagerHelper(this)
+
+        if (!permissionsManager.allPermissionsGranted()) {
+            permissionsManager.requestPermissions()
+        }
+        startCamera()
+
+        // Set up the listeners for take photo and video capture buttons
+        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+        viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun takePhoto() {}
+
+    // Implements VideoCapture use case, including start and stop capturing.
+    private fun captureVideo() {
+        val videoCapture = this.videoCapture ?: return
+
+
+        viewBinding.videoCaptureButton.isEnabled = false
+
+        val curRecording = recording
+        if (curRecording != null) {
+            // Stop the current recording session.
+            curRecording.stop()
+            recording = null
+            return
+        }
+
+        // create and start a new recording session
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
+            }
+        }
+
+        val mediaStoreOutputOptions = MediaStoreOutputOptions
+            .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+        recording = videoCapture.output
+            .prepareRecording(this, mediaStoreOutputOptions)
+            .apply {
+                if (PermissionChecker.checkSelfPermission(this@MainActivity,
+                        Manifest.permission.RECORD_AUDIO) ==
+                    PermissionChecker.PERMISSION_GRANTED)
+                {
+                    withAudioEnabled()
+                }
+            }
+            .start(ContextCompat.getMainExecutor(this)) { recordEvent ->
+                when(recordEvent) {
+                    is VideoRecordEvent.Start -> {
+                        recordingStartTime = System.currentTimeMillis() // 记录开始时间
+                        sensorManagerHelper.startSensorListener(sensorEventListener)// 启动传感器监听
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.stop_capture)
+                            isEnabled = true
+                        }
+                    }
+                    is VideoRecordEvent.Finalize -> {
+                        sensorManagerHelper.stopSensorListener(sensorEventListener)// 停止传感器监听
+                        recordingEndTime = System.currentTimeMillis() // 记录结束时间
+                        val recordingDuration = recordingEndTime - recordingStartTime // 计算录制时长
+                        if (!recordEvent.hasError()) {
+                            val msg = "Video capture succeeded: " +
+                                    "${recordEvent.outputResults.outputUri}, Duration: $recordingDuration ms"
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT)
+                                .show()
+                            Log.d(TAG, msg)
+                        } else {
+                            recording?.close()
+                            recording = null
+                            Log.e(TAG, "Video capture ends with error: " +
+                                    "${recordEvent.error}, Duration: $recordingDuration ms")
+                        }
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.start_capture)
+                            isEnabled = true
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+//            val qualitySelector = QualitySelector.from(Quality.HIGHEST, FallbackStrategy.higherQualityOrLowerThan(Quality.SD))
+            val recorder = Recorder.Builder()
+                .setQualitySelector(QualitySelector.from(Quality.HD))
+                .build()
+            videoCapture = VideoCapture.withOutput(recorder)
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider
+                    .bindToLifecycle(this, cameraSelector, preview, videoCapture)
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    private val sensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            if (recordingStartTime > 0) {
+                lifecycleScope.launch(Dispatchers.Default) { // 在后台线程处理数据
+                    val relativeTimeMillis = System.currentTimeMillis() - recordingStartTime
+                    val dataString = "${relativeTimeMillis},${event.values.joinToString(",")}\n"
+
+                    when (event.sensor.type) {
+                        Sensor.TYPE_ACCELEROMETER -> sensorManagerHelper.writeDataToFileAcc(dataString)
+                        Sensor.TYPE_GYROSCOPE -> sensorManagerHelper.writeDataToFileGyro(dataString)
+                        Sensor.TYPE_MAGNETIC_FIELD -> sensorManagerHelper.writeDataToFileMag(dataString)
+                    }
+                }
+
+            }
+
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+            // 可以在这里处理传感器精度变化，如果需要的话
+        }
+    }
+
+
+}
+
---
+++ ADtest_3/app/src/main/java/com/example/ad_test/SensorManagerHelper.kt
@@ -0,0 +1,147 @@
+package com.example.ad_test
+
+import android.content.ContentValues
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import java.io.IOException
+
+
+class SensorManagerHelper(private val context: Context) {
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+    private var gyroscope: Sensor? = null
+    private var magnetometer: Sensor? = null
+
+    private lateinit var accelerometerFileUri: Uri
+    private lateinit var gyroscopeFileUri: Uri
+    private lateinit var magnetometerFileUri: Uri
+    private lateinit var fileUri: Uri
+    private var addHeaderAcc: Boolean = true
+    private var addHeaderGyro: Boolean = true
+    private var addHeaderMag: Boolean = true
+
+
+    init {
+    }
+    private fun initializeSensors() {
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+
+        // 创建数据文件
+        //得到时间戳
+
+        accelerometerFileUri = createFileForSensor("accelerometer.csv")
+        gyroscopeFileUri = createFileForSensor("gyroscope.csv")
+        magnetometerFileUri = createFileForSensor("magnetometer.csv")
+        fileUri = createFileForSensor("file.csv")
+
+        addHeaderAcc = true
+        addHeaderGyro = true
+        addHeaderMag = true
+
+    }
+
+    fun startSensorListener(sensorEventListener: SensorEventListener) {
+        initializeSensors()
+        sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(sensorEventListener, gyroscope, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(sensorEventListener, magnetometer, SensorManager.SENSOR_DELAY_NORMAL)
+
+    }
+
+    fun stopSensorListener(sensorEventListener: SensorEventListener) {
+        sensorManager.unregisterListener(sensorEventListener)
+    }
+
+    fun createFileForSensor(fileName: String): Uri {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "text/csv")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+            }
+        }
+
+        val uri = context.contentResolver.insert(MediaStore.Files.getContentUri("external"), contentValues)
+        return uri ?: throw IOException("Failed to create new MediaStore record.")
+    }
+
+    fun writeDataToFileAcc(data: String) {
+        try {
+            context.contentResolver.openOutputStream(accelerometerFileUri, "wa")?.use { outputStream ->
+                if (addHeaderAcc) {
+                    // 只在首次写入时添加标题行
+                    val header = "Time,X-axis,Y-axis,Z-axis\n"
+                    outputStream.write(header.toByteArray())
+                    addHeaderAcc = false
+                }
+                // 假设data字符串是以逗号分隔的x,y,z值
+//                val csvFormattedString = "$data" // 在每条记录后添加换行符
+                outputStream.write(data.toByteArray())
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+    fun writeDataToFileGyro(data: String) {
+        try {
+            context.contentResolver.openOutputStream(gyroscopeFileUri, "wa")?.use { outputStream ->
+                if (addHeaderGyro) {
+                    // 只在首次写入时添加标题行
+                    val header = "Time,X-axis,Y-axis,Z-axis\n"
+                    outputStream.write(header.toByteArray())
+                    addHeaderGyro = false
+                }
+                // 假设data字符串是以逗号分隔的x,y,z值
+//                val csvFormattedString = "$data\n" // 在每条记录后添加换行符
+                outputStream.write(data.toByteArray())
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+    fun writeDataToFileMag(data: String) {
+        try {
+            context.contentResolver.openOutputStream(magnetometerFileUri, "wa")?.use { outputStream ->
+                if (addHeaderMag) {
+                    // 只在首次写入时添加标题行
+                    val header = "Time,X-axis,Y-axis,Z-axis,X-axis,Y-axis,Z-axis,X-axis,Y-axis,Z-axis\n"
+                    outputStream.write(header.toByteArray())
+                    addHeaderMag = false
+                }
+                // 假设data字符串是以逗号分隔的x,y,z值
+//                val csvFormattedString = "$data\n" // 在每条记录后添加换行符
+                outputStream.write(data.toByteArray())
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+    fun writeDataToFile(data: String) {
+        try {
+            context.contentResolver.openOutputStream(fileUri, "wa")?.use { outputStream ->
+                if (addHeaderMag) {
+                    // 只在首次写入时添加标题行
+                    val header = "Time,X-axis,Y-axis,Z-axis\n"
+                    outputStream.write(header.toByteArray())
+                    addHeaderMag = false
+                }
+                // 假设data字符串是以逗号分隔的x,y,z值
+//                val csvFormattedString = "$data\n" // 在每条记录后添加换行符
+                outputStream.write(data.toByteArray())
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+
+}
